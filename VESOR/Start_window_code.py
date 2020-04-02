f85f94b9b39bc5cb477fc2a071ca03d92bfae3e1
JASM <sierramendezjosealejandro@gmail.com>
2020-04-02 18:46:19 +0200

    Actualizacion del codigo, "Nuevas funciones"

--- VESOR/Start_window_code.py
+++ VESOR/Start_window_code.py
@@ -1,23 +1,30 @@
-from Start_window import *
-from PyQt5.QtWidgets import QApplication, QDialog, QMessageBox, QPushButton, QFrame
-from PyQt5.QtCore import Qt
-import sys
+import sys, re
+from PyQt5.QtWidgets import QApplication, QDialog, QMessageBox, QLineEdit
+from PyQt5 import uic
 
-class Start_window(Ui_Dialog):
+
+class Start_window(QDialog):
 	def __init__(self):
-		super().__init__()
-		self.setupUi(self)
-	############################################################################	
-		self.line_user.textChanged.connect(self.Validate_User)
+		QDialog.__init__(self)
+		uic.loadUi("Start_window.ui", self)
 
+##############################################################################################
+		self.line_user.textChanged.connect(self.Validate_user)
+		self.line_password.textChanged.connect(self.Validate_Password)
+		self.line_password.setEchoMode(QLineEdit.Password)   #Cambia los caracteres a puntos
+		self.line_password2.textChanged.connect(self.Validate_password2)
+		self.line_password2.setEchoMode(QLineEdit.Password)  #Cambia los caracteres a puntos 
+##############################################################################################
 
-
-	#############################################################################	
-	def Validate_User(self):
+##########################Declarando funciones para los lineEdit##############################
+	def Validate_user(self):
 		line_user = self.line_user.text()
-		validate = re.match('^[a-z\sáéíóúàèìòùäëïöüñ]+$', line_user, re.I)
+		validate = re.match('^[a-z\sáéíóúàèìòùäëïöüñ]+[0-9]+$', line_user, re.I)
 		if line_user == "":
 			self.line_user.setStyleSheet("border: 1px solid yellow;")
+			return False
+		elif len(line_user)<8:
+			self.line_user.setStyleSheet("border: 1px solid red;")
 			return False
 		elif not validate:
 			self.line_user.setStyleSheet("border: 1px solid red;")
@@ -26,16 +33,28 @@ class Start_window(Ui_Dialog):
 			self.line_user.setStyleSheet("border: 1px solid green;")
 			return True
 
+	def Validate_Password(self):
+		line_password = self.line_password.text()
+		validate = re.match('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$', line_password, re.I)
+		if line_password == "":
+			self.line_password.setStyleSheet("border: 1px solid yellow;")
+			return False
+		elif len(line_password)<8:
+			self.line_password.setStyleSheet("border: 1px solid red;")
+			return False
+		elif not validate:
+			self.line_password.setStyleSheet("border: 1px solid red;")
+			return False
+		else:
+			self.line_password.setStyleSheet("border: 1px solid green;")
+			return True
 
-if __name__ == '__main__':
-	app = QtWidgets.QApplication(sys.argv)
-	window = Start_window()
-	window.show()
-	app.exec_()
+	def Validate_password2(self):
+
+################################################################################################
 
 
-
-
-
-
-
+app = QApplication(sys.argv)
+startwindow = Start_window()
+startwindow.show()
+app.exec_()
