ab220cee92a8ac384c2c77fe9e98b3c83202f4cb
JASM <sierramendezjosealejandro@gmail.com>
2020-04-05 00:15:50 +0200

    "Codigo actualizado, mejorando la funciones"

--- VESOR/Start_window_code.py
+++ VESOR/Start_window_code.py
@@ -1,59 +1,86 @@
-##VESOR##
-
 import sys, re
-from PyQt5.QtWidgets import QApplication, QDialog, QMessageBox, QLineEdit
+from PyQt5.QtWidgets import QApplication, QDialog, QMessageBox, QLineEdit, QMainWindow, QAction
 from PyQt5 import uic
+from PyQt5.QtGui import QIcon
+import sqlite3
 
 
 class Start_window(QDialog):
-	def __init__(self):
-		QDialog.__init__(self)
-		uic.loadUi("Start_window.ui", self)
+    def __init__(self):
+        QDialog.__init__(self)
+        uic.loadUi("Start_window.ui", self)
 
-##############################################################################################
-		self.line_user.textChanged.connect(self.Validate_user)
-		self.line_password.textChanged.connect(self.Validate_Password)
-		self.line_password2.textChanged.connect(self.Validate_password2) 
-		self.Button_Cancel.clicked.connect(exit) #conecta con el boton para cerrar la aplicacion
-##############################################################################################
-		
-##########################Declarando funciones para los lineEdit##############################
-	def Validate_user(self):
-		line_user = self.line_user.text()
-		validate = re.match('^[a-z\sáéíóúàèìòùäëïöüñ]+[0-9]+$', line_user, re.I)
-		if line_user == "":
-			self.line_user.setStyleSheet("border: 1px solid yellow;")
-			return False
-		elif len(line_user)<8:
-			self.line_user.setStyleSheet("border: 1px solid red;")
-			return False
-		elif not validate:
-			self.line_user.setStyleSheet("border: 1px solid red;")
-			return False
-		else:
-			self.line_user.setStyleSheet("border: 1px solid green;")
-			return True
 
-	def Validate_Password(self):
-		line_password = self.line_password.text()
-		validate = re.match('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$', line_password, re.I) 
-		if line_password == "":
-			self.line_password.setStyleSheet("border: 1px solid yellow;")
-			return False
-		elif len(line_password)<8:
-			self.line_password.setStyleSheet("border: 1px solid red;")
-			return False
-		elif not validate:
-			self.line_password.setStyleSheet("border: 1px solid red;")
-			return False
-		else:
-			self.line_password.setStyleSheet("border: 1px solid green;")
-			return True
+        self.line_user.textChanged.connect(self.Data_user)
+        self.line_password.textChanged.connect(self.Data_password)
+        self.line_password2.textChanged.connect(self.Validate_password)
+        self.Button_register.clicked.connect(self.Login)
 
-	def Validate_password2(self):
-		# Puedes quitar el "pass" cuando quieras
-		pass 
-################################################################################################
+
+    #Variables:
+
+        #User = self.line_user.text()
+        #Password = self.line_password.text()
+        Password2 = self.line_password2.text()
+
+    def Data_user(self):
+        User = self.line_user.text()
+
+        validate = re.match('^[a-z\sáéíóúàèìòùäëïöüñ0-9]+$', User, re.I)
+
+        if len(User)<=8 or len(User)>=14 :
+            self.line_user.setStyleSheet("border: 1px solid red;")
+            return False
+
+        elif not validate:
+
+            self.line_user.setStyleSheet("border: 1px solid red;")
+            return False
+
+        else:
+            self.line_user.setStyleSheet("border: 1px solid green;")
+            return True
+    
+    def Data_password(self):
+        Password = self.line_password.text()
+        
+        validate = re.match('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$',Password, re.I)
+
+        if len(Password)<=8 or len(Password)>=12:
+            self.line_password.setStyleSheet("border: 1px solid red;")
+            return False
+
+        elif not validate:
+            self.line_password.setStyleSheet("border: 1px solid red;")
+            return False
+
+        else:
+            self.line_password.setStyleSheet("border: 1px solid green;")
+            return True
+    
+
+       
+    def Validate_password(self,Password2):
+
+        if Password2 != self.Data_password():
+            self.line_password2.setStyleSheet("border: 1px solid red;")
+            return False
+        else:
+            self.line_password2.setStyleSheet("border: 1px solid green;")
+            return True
+
+
+    def Login(self):
+        if self.Data_user() and self.Data_password() and self. Validate_password():
+            QMessageBox.information(self, "Registro", "Registro exitoso", QMessageBox.Yes)
+        else:
+            QMessageBox.warning(self, "Error", "Datos incorrecto", QMessageBox.Discard)
+        
+
+
+
+
+
 
 
 app = QApplication(sys.argv)
